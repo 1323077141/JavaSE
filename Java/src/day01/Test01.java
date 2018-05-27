cb58e77fa88f53da04bb6627f196f5970d2cc02a
1323077141 <1323077141@qq.com>
2018-05-27 20:53:59 +0800

    1.添加变量笔记

--- Java/src/day01/Test01.java
+++ Java/src/day01/Test01.java
@@ -1,8 +1,26 @@
 package day01;
 
+import com.sun.org.apache.bcel.internal.generic.SWAP;
+
 /**
  * Created by Administrator on 2018-05-22.
+ * 方法的参数传递
  */
 public class Test01 {
+    public static void main(String[] args){
+        Test01 t = new Test01();
+        int i = 10;
+        int j = 5;
+        System.out.println("i:" + i + " j:" + j);
 
+        t.swap(i,j);
+        System.out.println("i:" + i + " j:" + j);
+    }
+
+    //定义一个方法交换值
+    public void swap(int i ,int j){
+        int temp = i;
+        i = j;
+        j = temp;
+    }
 }
