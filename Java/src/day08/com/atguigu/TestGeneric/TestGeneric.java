7dcb1f81e0c35f275d12d8a5495b336473051bb5
1323077141 <1323077141@qq.com>
2018-06-17 21:20:07 +0800

    generic note achive

--- Java/src/day08/com/atguigu/TestGeneric/TestGeneric.java
+++ Java/src/day08/com/atguigu/TestGeneric/TestGeneric.java
@@ -10,10 +10,54 @@ import java.util.*;
  * 2.自定义泛型类，泛型接口，泛型方法
  * 3.泛型与继承的关系
  * 4.通配符
- *
+ *  PS：不可以在static,trycatch块中修饰符中使用泛型的声明
+ *  可以读取声明为通配符的集合类的对象
+ *  不可以向声明为通配符的集合类中写入对象，除了null值.
  *
  */
 public class TestGeneric {
+
+    /**
+     * 通配符 ?
+     * List<所有> 是List<?>的子类
+     *
+     *  ? extends A 可以存放A及其子类
+     *  ? super A 可以存放A及其父类
+     *
+     *
+     */
+    @Test
+    public void test6(){
+        List<?> list = null;
+        List<Object> list1 = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
+        list = list1;
+        list = list2;
+    }
+
+    /**
+     * 泛型与继承的关系
+     * 若A是B的子类，那么List<A>就不是List<B>的子接口
+     */
+    @Test
+    public void test5(){
+
+        Object obj = null;
+        String str = "AA";
+        obj = str;
+
+        Object [] obj1 = null;
+        String[] str1 = new String[]{"AA","BB","CC","DD"};
+        obj1 = str1;
+
+        List<Object> list = null;
+        List<String> list1 = new ArrayList<String>();
+//        list = list1;    错误
+
+
+
+    }
+
 
     //自定义泛型类的使用
     @Test
