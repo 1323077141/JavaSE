d9788b4e1e6b36c8d494d38ec03c762a8928af09
1323077141 <1323077141@qq.com>
2018-07-02 18:30:35 +0800

    note

--- Java/src/day08/com/atguigu/TestNetwork/TestTCP3.java
+++ Java/src/day08/com/atguigu/TestNetwork/TestTCP3.java
@@ -2,41 +2,49 @@ package day08.com.atguigu.TestNetwork;
 
 import org.junit.Test;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
- *
- * 客户端给服务端发消息，服务端将信息打印到控制台，同时发送“已收到”给客户端
- *
+ * 从客户端发送文件到服务端，服务端保存在本地，并返回"发送成功" 到客户端，并关闭相应的连接。
  */
-public class TestTCP2 {
+public class TestTCP3 {
 
     @Test
     public void client(){
         Socket client = null;
         OutputStream os = null;
+        FileInputStream fis = null;
         InputStream is = null;
         try {
             client = new Socket(InetAddress.getByName("127.0.0.1"),8989);
             os = client.getOutputStream();
-            os.write("我是客户端".getBytes());
-            //shutdownOutput : 显示的告诉服务器已发送完
+            fis = new FileInputStream(new File("1.jpg"));
+            byte[] b = new byte[20];
+            int len;
+            while ((len = fis.read(b)) != -1){
+                os.write(b,0,len);
+            }
             client.shutdownOutput();
             is = client.getInputStream();
-            byte[] b = new  byte[20];
-            int len;
-            while ((len = is.read(b)) != -1){
-                String str = new String(b,0,len);
+            byte[] b2 = new  byte[20];
+            int len2;
+            while ((len2= is.read(b2)) != -1){
+                String str = new String(b,0,len2);
                 System.out.println(str);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }finally {
+            if (fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
             if (is != null){
                 try {
                     is.close();
@@ -66,16 +74,18 @@ public class TestTCP2 {
         ServerSocket ss = null;
         Socket server = null;
         InputStream is = null;
+        FileOutputStream fos = null;
         OutputStream os = null;
         try {
             ss = new ServerSocket(8989);
             server = ss.accept();
             is = server.getInputStream();
+            fos = new FileOutputStream("2.jpg");
+
             byte[] b = new  byte[20];
             int len;
             while ((len = is.read(b)) != -1){
-                String str = new String(b,0,len);
-                System.out.println(str);
+                fos.write(b,0,len);
             }
 
             os = server.getOutputStream();
@@ -83,6 +93,13 @@ public class TestTCP2 {
         } catch (IOException e) {
             e.printStackTrace();
         }finally {
+            if (fos != null){
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
             if (is != null){
                 try {
                     is.close();
@@ -93,13 +110,6 @@ public class TestTCP2 {
             if (os != null){
                 try {
                     os.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (server != null){
-                try {
-                    server.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
